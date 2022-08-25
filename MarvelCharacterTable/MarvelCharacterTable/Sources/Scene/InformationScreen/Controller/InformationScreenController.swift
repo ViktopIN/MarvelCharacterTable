b5d7cfeebaad5f608dde70784e62d02e7e2cd448
Viktor <viktopiniaginya@gmail.com>
2022-08-25 13:28:36 +0300

    setup interaction between screens
    setup interaction between screens

---
+++ MarvelCharacterTable/MarvelCharacterTable/Sources/Scene/InformationScreen/Controller/InformationScreenController.swift
@@ -0,0 +1,36 @@
+//
+//  InformationScreenController.swift
+//  MarvelCharacterTable
+//
+//  Created by Виктор on 24/08/2022.
+//
+ import UIKit
+
+class InformationScreenController: UIViewController {
+
+//MARK: - Properties
+    var data: Character?
+
+    private var informationScreenView: InformationScreenView? {
+        guard isViewLoaded else { return nil }
+        return view as? InformationScreenView
+    }
+
+//MARK: - Lifecycle 
+
+    override func loadView() {
+        view = informationScreenView
+    }
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      commonInit()
+    }
+
+    //MARK: - Private functions -
+
+    private func commonInit() {
+        informationScreenView?.fillBlankData(data: data)
+    }
+}
+
---
+++ MarvelCharacterTable/MarvelCharacterTable/Sources/Scene/InformationScreen/Model/Model extension.swift
@@ -0,0 +1,8 @@
+//
+//  Model extension.swift
+//  MarvelCharacterTable
+//
+//  Created by Виктор on 24/08/2022.
+//
+
+import Foundation
